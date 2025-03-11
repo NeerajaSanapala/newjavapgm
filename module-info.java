ec01880236b6140fb61f3b54ff9e06613e5dd96a
Neeraja <neerajasanapala309@gmail.com>
2025-03-11 00:01:30 -0400

    newlyadded

---
+++ module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author neera
+ *
+ */
+module Project1 {
+}
---
+++ some/Random.java
@@ -0,0 +1,10 @@
+package some;
+
+public class Random {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Neeraja");
+	}
+
+}
